d3657b87e3f89d088acdf4e1023dcde2b117056e
shivam319 <46974184+shivam319@users.noreply.github.com>
2019-03-17 15:29:19 +0530

    Add files via upload

---
+++ Week-0/primenum.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+lower = int(input("Enter lower range: "))  
+upper = int(input("Enter upper range: "))  
+  
+for num in range(lower,upper + 1):  
+     if num > 1:  
+        for i in range(2,num):  
+            if (num % i) == 0:  
+                break  
+        else:  
+            print(num)  
+
+
+# In[ ]:
+
+
+
+
